dbb2a4311c340612c211a5017cd815dbc09e2997
JulianRODRIGUEZZ <116646357+JulianRODRIGUEZZ@users.noreply.github.com>
2023-10-20 00:25:21 -0500

    Add files via upload

---
+++ app-sena/index.php
@@ -0,0 +1,45 @@
+    <?php include("templates/header.php"); ?>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    
+    
+    
+    <div class="card mb-3" style="max-width: 880px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      
+    <img src="logo.jpg" class="img-fluid rounded-start" alt="logo.jpg">
+
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+      <h1 class="display-5 fw-bold">Sistema De Registro </br> En Etapa Productiva</h1>
+      <p class="col-md-8 fs-4">Realize la gestion de los aprendices en etapa productiva con sus respectivos programas de formacion.</p>
+          <button class="btn btn-success btn-lg" type="button"> Empezar</button>
+      </div>
+    </div>
+  </div>
+</div>
+<img src="logo.jpg" class="img-fluid rounded-start" alt="logo.jpg">
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    <!--   <div class="p-5 mb-4 bg-light rounded-3">
+        <div class="container-fluid py-5">
+          <h1 class="display-5 fw-bold">Sistema De Registro </br> En Etapa Productiva</h1>
+          <p class="col-md-8 fs-4">Realize la gestion de los aprendices en etapa productiva con sus respectivos programas de formacion.</p>
+          <button class="btn btn-success btn-lg" type="button"> Empezar</button>
+        </div>
+    </div> -->
+
+      <?php include("templates/footer.php"); ?>
